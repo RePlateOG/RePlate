eea63407e8ab39817f2ea2966a5d94ea00446e29
Jyotika Sadani <SJojo0716@gmail.com>
2026-01-24 19:18:11 -0600

    Initial Commit

---
+++ RePlate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RePlate
+//
+//  Created by Jyotika Sadani on 1/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RePlate/RePlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RePlateApp.swift
+//  RePlate
+//
+//  Created by Jyotika Sadani on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct RePlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
